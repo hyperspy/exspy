thomasaarholt/hyperspy

Add to_gpu and to_cpu method to BaseSignal.

hyperspy/_components/power_law.py
@@ -21,6 +21,7 @@ import logging
 
 from hyperspy._components.expression import Expression
 
+from hyperspy.misc.utils import get_numpy_kwargs
 
 _logger = logging.getLogger(__name__)
 
@@ -127,24 +128,22 @@ class PowerLaw(Expression):
         else:
             s = signal
         if s._lazy:
-            import dask.array as da
-            log = da.log
             I1 = s.isig[i1:i3].integrate1D(2j).data
             I2 = s.isig[i3:i2].integrate1D(2j).data
         else:
             from hyperspy.signal import BaseSignal
             shape = s.data.shape[:-1]
-            I1_s = BaseSignal(np.empty(shape, dtype='float'))
-            I2_s = BaseSignal(np.empty(shape, dtype='float'))
+            kw = get_numpy_kwargs(signal.data)
+            I1_s = BaseSignal(np.empty(shape, dtype='float', **kw))
+            I2_s = BaseSignal(np.empty(shape, dtype='float', **kw))
             # Use the `out` parameters to avoid doing the deepcopy
             s.isig[i1:i3].integrate1D(2j, out=I1_s)
             s.isig[i3:i2].integrate1D(2j, out=I2_s)
             I1 = I1_s.data
             I2 = I2_s.data
-            log = np.log
         with np.errstate(divide='raise'):
             try:
-                r = 2 * (log(I1) - log(I2)) / (log(x2) - log(x1))
+                r = 2 * (np.log(I1) - np.log(I2)) / (np.log(x2) - np.log(x1))
                 k = 1 - r
                 A = k * I2 / (x2 ** k - x3 ** k)
                 if s._lazy:
@@ -157,15 +156,15 @@ class PowerLaw(Expression):
                 _logger.warning('Power-law parameter estimation failed '
                                 'because of a "divide-by-zero" error.')
                 return False
+
         if only_current is True:
             self.r.value = r
             self.A.value = A
             return True
+
         if out:
             return A, r
         else:
-            if self.A.map is None:
-                self._create_arrays()
             self.A.map['values'][:] = A
             self.A.map['is_set'][:] = True
             self.r.map['values'][:] = r

hyperspy/exspy

hyperspy/_components/power_law.py
@@ -22,6 +22,7 @@ import numpy as np
 
 from hyperspy._components.expression import Expression
 
+from hyperspy.misc.utils import get_numpy_kwargs
 
 _logger = logging.getLogger(__name__)
 
@@ -132,24 +133,22 @@ class PowerLaw(Expression):
         else:
             s = signal
         if s._lazy:
-            import dask.array as da
-            log = da.log
             I1 = s.isig[i1:i3].integrate1D(2j).data
             I2 = s.isig[i3:i2].integrate1D(2j).data
         else:
             from hyperspy.signal import BaseSignal
             shape = s.data.shape[:-1]
-            I1_s = BaseSignal(np.empty(shape, dtype='float'))
-            I2_s = BaseSignal(np.empty(shape, dtype='float'))
+            kw = get_numpy_kwargs(s.data)
+            I1_s = BaseSignal(np.empty(shape, dtype='float', **kw))
+            I2_s = BaseSignal(np.empty(shape, dtype='float', **kw))
             # Use the `out` parameters to avoid doing the deepcopy
             s.isig[i1:i3].integrate1D(2j, out=I1_s)
             s.isig[i3:i2].integrate1D(2j, out=I2_s)
             I1 = I1_s.data
             I2 = I2_s.data
-            log = np.log
         with np.errstate(divide='raise'):
             try:
-                r = 2 * (log(I1) - log(I2)) / (log(x2) - log(x1))
+                r = 2 * (np.log(I1) - np.log(I2)) / (np.log(x2) - np.log(x1))
                 k = 1 - r
                 A = k * I2 / (x2 ** k - x3 ** k)
                 if s._lazy:
@@ -162,15 +161,15 @@ class PowerLaw(Expression):
                 _logger.warning('Power-law parameter estimation failed '
                                 'because of a "divide-by-zero" error.')
                 return False
+
         if only_current is True:
             self.r.value = r
             self.A.value = A
             return True
+
         if out:
             return A, r
         else:
-            if self.A.map is None:
-                self._create_arrays()
             self.A.map['values'][:] = A
             self.A.map['is_set'][:] = True
             self.r.map['values'][:] = r
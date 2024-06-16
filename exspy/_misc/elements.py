aa6ce5c3bb856a54e3c780e0ccea5b6cd103f6e9
Eric Prestat <eric.prestat@gmail.com>
2024-06-16 13:04:42 +0100

    Fix `exspy.material.elements` docstring and add it to the API reference

--- exspy/_misc/elements.py
+++ exspy/_misc/elements.py
@@ -4979,6 +4979,51 @@ elements = {
 }
 
 elements_db = DictionaryTreeBrowser(elements)
+elements_db.__doc__ = """
+Database of element properties.
+
+The following properties are included:
+
+.. code::
+
+    ├── Atomic_properties
+    │   ├── Binding_energies
+    │   └── Xray_lines
+    ├── General_properties
+    │   ├── Z
+    │   ├── atomic_weight
+    │   └── name
+    └── Physical_properties
+        └── density_gcm3
+
+Examples
+--------
+>>> exspy.material.elements.Fe.General_properties
+├── Z = 26
+├── atomic_weight = 55.845
+└── name = iron
+>>> exspy.material.elements.Fe.Physical_properties
+└── density (g/cm^3) = 7.874
+>>> exspy.material.elements.Fe.Atomic_properties.Xray_lines
+├── Ka
+│   ├── energy (keV) = 6.404
+│   └── weight = 1.0
+├── Kb
+│   ├── energy (keV) = 7.0568
+│   └── weight = 0.1272
+├── La
+│   ├── energy (keV) = 0.705
+│   └── weight = 1.0
+├── Lb3
+│   ├── energy (keV) = 0.792
+│   └── weight = 0.02448
+├── Ll
+│   ├── energy (keV) = 0.615
+│   └── weight = 0.3086
+└── Ln
+    ├── energy (keV) = 0.62799
+    └── weight = 0.12525
+"""
 
 # read dictionary of atomic numbers from eXSpy, and add the elements that
 # do not currently exist in the database (in case anyone is doing EDS on
